c07a596f84816422bbcef77355677e9039546473
gi0210lee <gi0210lee@gmail.com>
2022-05-24 17:45:20 +0900

    update Header

---
+++ components/Header.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import React from "react";
+import {
+  BeakerIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+  StarIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <div className="flex  bg-white px-4 shadow-sm sticky top-0 z-50">
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          priority={true}
+          objectFit="contain"
+          src="https://logos-world.net/wp-content/uploads/2020/10/Reddit-Logo.png"
+          layout="fill"
+          alt=""
+        />
+      </div>
+
+      <div className="flex items-center mx-7 xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="flex-1 ml-2 hidden lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+
+      {/* Search Box */}
+      <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
+
+      <div className="flex">
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
